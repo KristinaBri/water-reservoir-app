060d311db86f09b6aa01bc20433d3585fe387a03
KristinaBri <59978364+KristinaBri@users.noreply.github.com>
2022-10-18 17:18:47 +0300

    Update

--- src/app/components/water/water.component.ts
+++ src/app/components/water/water.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./water.component.css']
 })
 export class WaterComponent implements OnInit {
+  public capacity:number|null=null;
+  public speed:number|null=null;
+  public duration:number|null=null;
+  public water:number|null=null;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public calculate(){
+    if (this.capacity!=null && this.speed!=null && this.duration!=null){
+      this.water=Number( (this.capacity/((this.duration*this.speed)*100)));
+    }
   }
 
 }
